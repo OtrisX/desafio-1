48643286eb4972c2d2bb1edc95f08fec5d6f5f5f
OtrisX <theliomatheus@protonmail.com>
2022-08-28 11:01:24 -0300

    : )

--- alunosClass.js
+++ alunosClass.js
@@ -1,3 +1,6 @@
+const fs = require("fs")
+
+
 module.exports = class Alunos {
     constructor() {
         this.alunos = []
@@ -5,6 +8,25 @@ module.exports = class Alunos {
     novoAluno(nome, matricula, telefone, email, uffmail, status) {
         let aluno = {"nome": nome, "matricula": matricula, "telefone": telefone, "email":email, "uffmail": uffmail, "status": status}
         this.alunos.push(aluno)
+    }
+    lerCsv(path){
+        let alunosRaw = fs.readFileSync(path, "utf-8")
+        let alunosLinesArray = alunosRaw.split(/\r\n|\r|\n/)
+        let alunosArray = []
+        alunosLinesArray.forEach(e => {
+            alunosArray.push(e.split(","))
+        })
+        alunosArray.forEach(aluno => {
+            this.novoAluno(
+            aluno[0],
+            aluno[1],
+            aluno[2],
+            aluno[3],
+            aluno[4],
+            aluno[5]
+        )
+    })
+        
     }
     pesquisarAluno(matricula){
         return this.alunos.find(e => e.matricula === matricula)
@@ -20,8 +42,5 @@ module.exports = class Alunos {
             "4": `${nome[0][0]}${nome[2]}@id.uff.br`,
             "5": `${nome[2]}_${nome[0]}@id.uff.br`,
         }
-    }
-    editarAluno(){
-
     }
 }
--- script.js
+++ script.js
@@ -2,73 +2,41 @@ const Alunos = require("./alunosClass")
 const fs = require("fs")
 const axios = require("axios")
 const prompt = require("prompt-sync")()
+var colors = require('colors');
 
-// Criar a classe alunos :)
-const alunos = new Alunos
-
-
-// Fazer a requisição dos dados dos alunos
-
-// function getAlunos() {
-//     return axios.get("https://raw.githubusercontent.com/sti-uff/trabalhe-conosco/master/datasets/alunos.csv")
-//         .then((res) => {
-//             return res.data
-//         })
-//         .catch((err) => {
-//             console.error(err)
-//         })
-// }
-
-function getAlunos() {
-    return fs.readFileSync("./alunos.csv", "utf-8")
-}
-
-// Formatar o array de alunos recebido pela requisição e cadastrar os alunos na classe Alunos
-async function alunosFormatter() {
-    let alunosRaw = await getAlunos() // await seria caso fosse uma requisição http, vou deixar pq não faz diferença
-    let alunosLinesArray = alunosRaw.split(/\r\n|\r|\n/)
-    let alunosArray = []
-    alunosLinesArray.forEach(e => {
-        alunosArray.push(e.split(","))
-    })
-    alunosArray.forEach(aluno => {
-        alunos.novoAluno(
-            aluno[0],
-            aluno[1],
-            aluno[2],
-            aluno[3],
-            aluno[4],
-            aluno[5]
-        )
-    })
-}
-
-async function main() {
-    await alunosFormatter()     
-
-    console.log()
-
-    let matricula = prompt("Digite sua matrícula: ")
+function main() {
+    // Iniciar a classe alunos :)
+    const alunos = new Alunos
+    // Ler o arquivo .csv e adicionar os alunos a classe
+    alunos.lerCsv("./alunos.csv")
+    // Coletar os dados do aluno a partir da matrícula e gerar os possiveis emails
+    let matricula = prompt("Digite sua matrícula: ".bold.white)
+    
     let aluno = alunos.pesquisarAluno(matricula)
     let emails = alunos.gerarEmailValido(matricula)
-    let nomeAluno = alunos.pesquisarAluno(matricula).nome.split(" ")
+    let nomeAluno = aluno.nome.split(" ")[0]
 
-    if(aluno.status === "Ativo"){
+    //  Questionario e verificar se aluno é ativo ou não
+    if(aluno.status === "Ativo" && aluno.uffmail.length === 0){
         // Identação ficou bizarra pra sair direito no terminal
         console.log(`
-        \n${nomeAluno[0]}, por favor escolha uma das opções abaixo para o seu UFFMail\n
+        \n${nomeAluno}, por favor escolha uma das opções abaixo para o seu UFFMail\n
     1 - ${emails["1"]}
     2 - ${emails["2"]}
     3 - ${emails["3"]}
     4 - ${emails["4"]}
-    5 - ${emails["5"]}\n`)
+    5 - ${emails["5"]}\n`.bold.white)
     
-        let emailOpc = prompt("Digite um número: ")
+        let emailOpc = prompt("Digite um número: ".bold.white)
     
         console.log(`\nA criação de seu e-mail (${emails[emailOpc]}) será feita nos próximos minutos.
-Um SMS foi enviado para ${aluno.telefone} com a sua senha de acesso.`)   
-    } else{
-        console.error("Aluno inativo.")
+Um SMS foi enviado para ${aluno.telefone} com a sua senha de acesso.`.bold.green)   
+    } 
+    if(aluno.status === "Inativo") {
+        console.error("Aluno inativo".bold.red)
+    }
+    if(aluno.uffmail.length > 0) {
+        console.error("Aluno já possui uffmail".bold.red)
     }
 
     
